1d6fdd7d3530e85e2de083c258d80b0f9dabf7e2
Gustavo Arendt <gustavo.arendt@outlook.com>
2020-12-16 23:00:17 -0300

    discord bot files without node and config

---
+++ aDiscBot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let regList = [];
+client.on('message', msg => {
+
+
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'help') {
+    msg.channel.send('Olá, utilize `a!list` para verificar a lista de pessoas interessadas em jogar ou `a!register` para mostrar interesse!!');
+  } else if (command === 'register') {
+      if(regList.includes(msg.author.username)) {
+          return msg.channel.send('Você já está registrado na lista de interesse!');
+      }
+      msg.channel.send(`${msg.author.username} acabou de demonstrar interesse em jogar. utilize "a!leave" para sair da lista`);
+      regList.push(msg.author.username);
+  }
+
+  else if (command === 'leave') {
+    if(!regList.includes(msg.author.username)) {
+        return msg.channel.send('Você não está registrado na lista de interesse!');
+    }
+    regList = regList.filter(item => item !== msg.author.username);
+    msg.channel.send(`${msg.author.username} deixou a lista de interesse. utilize "a!register" caso queira retornar`);
+  }
+
+  else if (command === 'list') {
+    if(regList.length === 0) {
+        return msg.channel.send('A lista de interesse está vazia. Utilize `a!register` caso queira participar.')
+    }
+    msg.channel.send('Usuários com interesse em jogar:');
+    regList.forEach(user => msg.channel.send(`${regList.indexOf(user)+1} -> ${user}`));
+    console.log(`${regList} + ${msg.author.tag} + ${msg.author.avatar} + ${msg.author.id}`);
+  }
+
+  else if (command === 'mute' && msg.member.hasPermission("MANAGE_ROLES", "ADMINISTRATOR")) {
+      msg.channel.send(`${msg.member.voice.channel.name} está mutado.`);
+      msg.member.voice.channel.members.forEach(member => member.voice.setMute(true));
+  } else if (command === 'unmute' && msg.member.hasPermission("MANAGE_ROLES", "ADMINISTRATOR")) {
+      msg.channel.send(`${msg.member.voice.channel.name} está desmutado.`);
+      msg.member.voice.channel.members.forEach(member => member.voice.setMute(false));
+  }
+
+});
+
+client.login(token);
